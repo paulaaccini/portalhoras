13302f4c990b0415cb7ac2fe4f4fa8d54903c496
Paula Accini <paulaaccini@Mac.lan>
2026-08-05 18:26:21 -0500

    fix prot

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const { Pool } = require('pg');
 const fs = require('fs');
 
 const app  = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 const UPLOADS_DIR = path.join(__dirname, 'uploads');
 if (!fs.existsSync(UPLOADS_DIR)) fs.mkdirSync(UPLOADS_DIR, { recursive: true });
